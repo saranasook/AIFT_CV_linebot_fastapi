d754bc11e742ffd2045babc2e6b7424ed3d003b7
saranasook <ohm.sarana@gmail.com>
2025-02-04 09:02:32 +0700

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,1 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import app.service as service
 
+app = FastAPI(
+    title="aiforthai-line-chatbot",
+    description="AIFORTHAI LINE CHATBOT WORKSHOP",
+    version="1.0.0",
+)
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(service.router)
+
+
+@app.get("/")
+def index():
+    return "AIFORTHAI LINE CHATBOT WORKSHOP"
